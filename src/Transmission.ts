2e969f355a867c73ceeac8f43709ed05dcaa0ee0
Brian Vo <brianhvo02@gmail.com>
2023-11-09 23:54:20 -0800

    add torrent get

--- src/Transmission.ts
+++ src/Transmission.ts
@@ -62,6 +62,15 @@ export default class Transmission {
 
     torrentRemove = async (options: Partial<TorrentRemoveArgs> = {}) =>
         this.fetch<TMResponse<{}>, typeof options>('torrent-remove', options);
+
+    async torrentGet<T extends TorrentInfoKeys>(fields: T[], options?: TorrentGetOptions) {
+        const args = {
+            fields: [...new Set(fields)],
+            ...options
+        };
+
+        return this.fetch<TMResponse<TorrentGetResponse<T>>, typeof args>('torrent-get', args);
+    }
 }
 
 export interface TMResponse<T> {
@@ -105,4 +114,185 @@ export interface TorrentAddResponseInfo {
 export interface TorrentRemoveArgs {
     ids: number | number[];
     deleteLocalData: boolean;
+}
+
+export interface TorrentGetArgs {
+    fields: TorrentInfoKeys[];
+    ids: number[];
+}
+
+export type TorrentGetOptions = Partial<Omit<TorrentGetArgs, 'fields'>>;
+
+export type TorrentGetResponse<T extends TorrentInfoKeys> = Pick<TorrentInfo, T>[];
+
+export type TorrentInfoKeys = keyof TorrentInfo;
+export interface TorrentInfo {
+    activityDate: number;
+    addedDate: number;
+    availability: number[];
+    bandwidthPriority: number;
+    comment: string;
+    corruptEver: number;
+    creator: string;
+    dateCreated: number;
+    desiredAvailable: number;
+    doneDate: number;
+    downloadDir: string;
+    downloadedEver: number;
+    downloadLimit: number;
+    downloadLimited: boolean;
+    editDate: number;
+    error: number;
+    errorString: string;
+    eta: number;
+    etaIdle: number;
+    fileCount: number;
+    files: File[];
+    fileStats: FileStats[];
+    group: string;
+    hashString: string;
+    haveUnchecked: number;
+    haveValid: number;
+    honorsSessionLimits: boolean;
+    id: number;
+    isFinished: boolean;
+    isPrivate: boolean;
+    isStalled: boolean;
+    labels: string[];
+    leftUntilDone: number;
+    magnetLink: string;
+    manualAnnounceTime: number;
+    maxConnectedPeers: number;
+    metadataPercentComplete: number;
+    name: string;
+    peerLimit: number;
+    peers: Peer[];
+    peersConnected: number;
+    peersFrom: PeersFrom;
+    peersGettingFromUs: number;
+    peersSendingToUs: number;
+    percentComplete: number;
+    percentDone: number;
+    pieces: string;
+    pieceCount: number;
+    pieceSize: number;
+    priorities: number[];
+    primaryMimeType: string;
+    queuePosition: number;
+    rateDownload: number;
+    rateUpload: number;
+    recheckProgress: number;
+    secondsDownloading: number;
+    secondsSeeding: number;
+    seedIdleLimit: number;
+    seedIdleMode: number;
+    seedRatioLimit: number;
+    seedRatioMode: number;
+    sequentialDownload: boolean;
+    sizeWhenDone: number;
+    startDate: number;
+    status: Status;
+    trackers: Tracker[];
+    trackerList: string;
+    trackerStats: TrackerStats[];
+    totalSize: number;
+    torrentFile: string;
+    uploadedEver: number;
+    uploadLimit: number;
+    uploadLimited: boolean;
+    uploadRatio: number;
+    wanted: number[];
+    webseeds: string[];
+    webseedsSendingToUs: number;
+}
+
+export interface File {
+    bytesCompleted: number;
+    length: number;
+    name: string;
+    beginPiece: number;
+    endPiece: number;
+}
+
+export interface FileStats {
+    bytesCompleted: number;
+    wanted: 0 | 1;
+    priority: number;
+}
+
+export interface Peer {
+    address: string;
+    clientName: string;
+    clientIsChoked: boolean;
+    clientIsInterested: boolean;
+    flagStr: string;
+    isDownloadingFrom: boolean;
+    isEncrypted: boolean;
+    isIncoming: boolean;
+    isUploadingTo: boolean;
+    isUTP: boolean;
+    peerIsChoked: boolean;
+    peerIsInterested: boolean;
+    port: number;
+    progress: number;
+    rateToClient: number;
+    rateToPeer: number;
+}
+
+export interface PeersFrom {
+    fromCache: number;
+    fromDht: number;
+    fromIncoming: number;
+    fromLpd: number;
+    fromLtep: number;
+    fromPex: number;
+    fromTracker: number;
+}
+
+export enum Status {
+    STOPPED,
+    VERIFY_QUEUE,
+    VERIFY,
+    DOWNLOAD_QUEUED,
+    DOWNLOAD,
+    SEED_QUEUED,
+    SEED
+}
+
+export interface Tracker {
+    announce: string;
+    id: number;
+    scrape: string;
+    sitename: string;
+    tier: number;
+}
+
+export interface TrackerStats {
+    announceState: number;
+    announce: string;
+    downloadCount: number;
+    hasAnnounced: boolean;
+    hasScraped: boolean;
+    host: string;
+    id: number;
+    isBackup: boolean;
+    lastAnnouncePeerCount: number;
+    lastAnnounceResult: string;
+    lastAnnounceStartTime: number;
+    lastAnnounceSucceeded: boolean;
+    lastAnnounceTime: number;
+    lastAnnounceTimedOut: boolean;
+    lastScrapeResult: string;
+    lastScrapeStartTime: number;
+    lastScrapeSucceeded: boolean;
+    lastScrapeTime: number;
+    lastScrapeTimedOut: boolean;
+    leecherCount: number;
+    nextAnnounceTime: number;
+    nextScrapeTime: number;
+    scrapeState: number;
+    scrape: string;
+    seederCount: number;
+    sitename: string;
+    tier: number;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import Transmission from "./Transmission.js";
 
 const client = new Transmission('localhost', 9091, '/transmission/');
-// const addRes = await client.torrentAdd('https://nyaa.si/download/1739865.torrent');
-// console.log(addRes);
-// const removeRes = await client.torrentRemove({ ids: 6 });
-// console.log(removeRes);
+const addRes = await client.torrentAdd('https://nyaa.si/download/1739865.torrent');
+console.log(addRes);
+const getRes = await client.torrentGet(['id', 'name']);
+console.log(getRes.arguments);
+const removeRes = await client.torrentRemove({ ids: addRes.arguments.id, deleteLocalData: true });
+console.log(removeRes);
